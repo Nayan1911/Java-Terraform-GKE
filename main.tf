f56d20aa4fe0e2c391f19d2a210aaaa9fd45b2e1
Nayan1911 <sharmanayan39@gmail.com>
2024-05-14 17:57:00 +0530

    Final Commit

--- main.tf
+++ main.tf
@@ -1,111 +1,54 @@
 provider "google" {
-  project     = var.project_id
-  region      = var.region
-  zone        = var.zone
+    project     = "glowing-palace-414116"
+    region      = "us-central1"
+    zone        = "us-central1-c"
 }
 
-resource "google_container_cluster" "cluster" {
-  name               = var.cluster_name
-  location           = var.zone
-  initial_node_count = 1
+resource "google_compute_network" "default" {
+  name = "example-network"
 
-  node_config {
-    machine_type = "n1-standard-1"
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/compute",
-      "https://www.googleapis.com/auth/devstorage.read_only",
-      "https://www.googleapis.com/auth/logging.write",
-      "https://www.googleapis.com/auth/monitoring"
-    ]
-  }
-
-  timeouts {
-    create = "30m"
-    update = "30m"
-  }
+  auto_create_subnetworks  = false
+  enable_ula_internal_ipv6 = true
 }
 
-provider "kubernetes" {
-  load_config_file = false
-  host             = "https://${google_container_cluster.cluster.endpoint}"
-  token            = var.kubernetes_token
-  # Set other authentication parameters as needed
-}
+resource "google_compute_subnetwork" "default" {
+  name = "example-subnetwork"
 
-resource "kubernetes_deployment" "hello_world_deployment" {
-  metadata {
-    name      = "hello-world-deployment"
-    namespace = "default"
+  ip_cidr_range = "10.0.0.0/16"
+  region        = "us-central1"
+
+  stack_type       = "IPV4_IPV6"
+  ipv6_access_type = "INTERNAL" # Change to "EXTERNAL" if creating an external loadbalancer
+
+  network = google_compute_network.default.id
+  secondary_ip_range {
+    range_name    = "services-range"
+    ip_cidr_range = "192.168.0.0/24"
   }
 
-  spec {
-    selector {
-      match_labels = {
-        app = "hello-world"
-      }
-    }
-
-    template {
-      metadata {
-        labels = {
-          app = "hello-world"
-        }
-      }
-
-      spec {
-        container {
-          image = "sharmanayan/hello-world:0.1.RELEASE"
-          name  = "hello-world"
-          ports {
-            container_port = 8080
-          }
-        }
-      }
-    }
+  secondary_ip_range {
+    range_name    = "pod-ranges"
+    ip_cidr_range = "192.168.1.0/24"
   }
 }
 
-resource "kubernetes_service" "hello_world_service" {
-  metadata {
-    name      = "hello-world-service"
-    namespace = "default"
+resource "google_container_cluster" "default" {
+  name = "example-autopilot-cluster"
+
+  location                 = "us-central1"
+  enable_autopilot         = true
+  enable_l4_ilb_subsetting = true
+
+  network    = google_compute_network.default.id
+  subnetwork = google_compute_subnetwork.default.id
+
+  ip_allocation_policy {
+    stack_type                    = "IPV4_IPV6"
+    services_secondary_range_name = google_compute_subnetwork.default.secondary_ip_range[0].range_name
+    cluster_secondary_range_name  = google_compute_subnetwork.default.secondary_ip_range[1].range_name
   }
 
-  spec {
-    selector = {
-      app = "hello-world"
-    }
-
-    port {
-      protocol   = "TCP"
-      port       = 80
-      target_port = 8080
-    }
-
-    type = "LoadBalancer"
-  }
-}
-
-resource "kubernetes_ingress" "hello_world_ingress" {
-  metadata {
-    name      = "hello-world-ingress"
-    namespace = "default"
-    annotations = {
-      "kubernetes.io/ingress.class" = "nginx"
-    }
-  }
-
-  spec {
-    rule {
-      http {
-        path {
-          path = "/"
-          backend {
-            service_name = kubernetes_service.hello_world_service.metadata[0].name
-            service_port = kubernetes_service.hello_world_service.spec[0].port[0].port
-          }
-        }
-      }
-    }
-  }
+  # Set `deletion_protection` to `true` will ensure that one cannot
+  # accidentally delete this instance by use of Terraform.
+  deletion_protection = false
 }
